94880245012da2354a73ddfda6e2db6a5f475479
Adriano Nobre <adriano.nbfr@gmail.com>
2022-12-29 14:19:48 -0300

    Alteração do título

--- hello/src/app/app.component.ts
+++ hello/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angulapp';
+  title = 'Angular App Hello';
   autor = 'Adriano';
 }
