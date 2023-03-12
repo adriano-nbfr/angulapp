e0a23061a219f993830dfa18cceb7976f1930ddb
Adriano Nobre <adriano.nbfr@gmail.com>
2023-03-12 20:43:26 -0300

    Alguns ajustes

--- hello/src/app/app.component.ts
+++ hello/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hello Angular 5';
+  title = 'Hello Angular';
   autor = 'Adriano Nobre';
 }
